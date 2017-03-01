4fd9aeb886b6514d1f5b8d1eec0213fe481a9041
Xiaoqian <xiaoqian.liu@salesforce.com>
2017-03-01 00:31:28 -0800

    Update nn_example.py

--- nn_example.py
+++ nn_example.py
@@ -5,14 +5,15 @@ from graph import *
 W = Node(1,0,"w")
 x = Node(2,0, "x")
 b = Node(1,0, "b")
-y = Node(3,0,"y")
+y = Node(1,0,"y")
 
 g1 = multiplyGate(W,x)
 g2 = sumGate(g1, b)
+#first layer
 g3 = sigmoidGate(g2)
-
+#second layer
 g4 = sigmoidGate(g3)
-
+#final layer
 g5 = softmaxLossGate(g4)
 
 g5.next = y
@@ -20,6 +21,8 @@ g5.next = y
 ppl  = Pipeline([g3, g4, g2, g1,g5])
 order =  ppl.topologicalSort()
 
+
+# each iteration: forward, backprop, optimization
 #forward pop
 for gate in order:
 	gate.forward()
@@ -30,6 +33,51 @@ for gate in order:
 gradient = 0
 for gate in order[::-1]:
 	#the last node
-	gate.backward(gate.next)
-	print gate.label
-	print gate.out.val
+	gate.backward()
+
+
+
+a = Node(1, 0, "a")
+b = Node(2, 0, "b")
+c = Node(-3, 0, "c")
+x = Node(-1, 0, "x")
+y = Node(3,0, "y")
+
+g1 = multiplyGate(a,x)
+g2 = multiplyGate(b,y)
+g3 = sumGate(g1, g2)
+g4 = sumGate(g3, c)
+g5 = sigmoidGate(g4)
+
+weights = [a,b,c,x,y]
+ppl  = Pipeline([g3, g4, g2, g1,g5])
+order =  ppl.topologicalSort()
+
+for gate in order:
+	gate.forward()
+
+print 'circuit output: ', getVal(g5.out) #0.8808 yay
+
+out = Node(1,0,"out")
+g5.out.grad = 1
+for gate in order[::-1]:
+	#the last node
+	gate.backward()
+
+#gradient descent
+step_size = 0.01
+a.val += step_size*a.grad
+print a.grad
+b.val += step_size*b.grad
+print b.grad
+c.val += step_size*c.grad
+print c.grad
+x.val += step_size*x.grad
+print x.grad
+y.val += step_size*y.grad
+print y.grad
+for gate in order:
+	gate.forward()
+
+print 'circuit output: ', getVal(g5.out)
+
