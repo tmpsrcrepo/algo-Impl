4d40943e3192063bef574da93610b32676fd7b4f
Xiaoqian <xiaoqian.liu@salesforce.com>
2017-03-01 22:06:04 -0800

    Create example.py

---
+++ example.py
@@ -0,0 +1,32 @@
+#test example from Karpathy's website
+
+a = Node(1, 0, "a")
+b = Node(2, 0, "b")
+c = Node(-3, 0, "c")
+x = Node(-1, 0, "x")
+y = Node(3,0, "y")
+
+g1 = multiplyGate(a,x)
+g2 = multiplyGate(b,y)
+g3 = sumGate(g1, g2)
+g4 = sumGate(g3, c)
+g5 = sigmoidGate(g4)
+
+
+weights = [a,b,c,x,y]
+training2 = Training(gateList = [g3, g4, g2, g1,g5], weights = [a,b,c,x,y])
+
+#forward prop
+training2.forwardTraining(0)
+print 'circuit output: ', getVal(g5.out) #0.8808 yay
+
+# backprop: start from end
+g5.out.grad = 1
+training2.backwardTraining(0)
+
+#gradient descent for each weight vector
+training2.gradientDescent(0.01)
+
+training2.forwardTraining(0)
+
+print 'circuit output: ', getVal(g5.out) #0.8826 woohoo
